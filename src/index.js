e844007a5aa9475edf1be09898519e6c1bda3d42
tmoechel-futura <t.moechel@futura-solutions.de>
2023-12-16 10:01:47 +0100

    setting styles and element props

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function App() {
-  return <h2> Hello React </h2>;
+  return (
+    <div>
+      <textarea autoFocus={true} style={{ backgroundColor: "azure" }} />
+      <textarea
+        readOnly={true}
+        maxLength= {3}
+        spellCheck={true}
+        style= {{ backgroundColor:"gray"}}      />
+    </div>
+  );
 }
 
 root.render(<App />);
