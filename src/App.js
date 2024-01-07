fb6778190106611b26d1fba000fe33330fdf1ac8
tmoechel-futura <t.moechel@futura-solutions.de>
2024-01-07 21:22:31 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import SearchBar from "./components/SearchBar";
-import ImageList from "./components/ImageList";
-
-import getImages from "./api";
-import { useState } from "react";
-
 function App() {
-  const [imageList, setImageList] = useState([]);
-
-  const handleSearch = async (term) => {
-    const images = await getImages(term);
-    setImageList(images);
-  };
-
   return (
     <div>
-      <SearchBar onSearch={handleSearch} />
-      <ImageList images={imageList} />
+      <h2> Hello React !</h2>
     </div>
   );
 }
