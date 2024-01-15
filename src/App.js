388b0e16d738e6c14ec6c1be8613b0d289ed3b6d
tmoechel-futura <t.moechel@futura-solutions.de>
2024-01-15 19:40:11 +0100

    flex-box

--- src/App.js
+++ src/App.js
@@ -1,43 +1,14 @@
 import { useState } from "react";
 
-import ShowAnimal from "./components/ShowAnimal";
-
-function getRandomAnimal() {
-  const imgArray = [
-    "imgBird",
-    "imgCat",
-    "imgCow",
-    "imgDog",
-    "imgGator",
-    "imgHorse",
-  ];
-  const index = Math.floor(Math.random() * imgArray.length);
-  return imgArray[index];
-}
-
 function App() {
-  const [animals, setAnimals] = useState([]);
-
-  function handleAddAnimalClick() {
-    setAnimals([...animals, getRandomAnimal()]);
-  }
-
   return (
-    <div>
-      <button
-        className="btn bg-gray-200 mt-3 hover:shadow-inner"
-        onClick={handleAddAnimalClick}
-      >
-        {" "}
-        Add Animal{" "}
-      </button>
-      <div>
-        <div className="md:flex md:justify-start md:items-center flex-wrap">
-          {animals.map((animal, index) => {
-            return <ShowAnimal type={animal} key={index} />;
-          })}
-        </div>
+    <div className="">
+      <div className="bg-blue-400">Header</div>
+      <div className="flex flex-col md:flex-row">
+        <div className="bg-green-400 flex-none min-w-48">Sidebar</div>
+        <div className="bg-red-400 flex-grow">Main Content</div>
       </div>
+      <div className="bg-blue-400">Footer</div>
     </div>
   );
 }
