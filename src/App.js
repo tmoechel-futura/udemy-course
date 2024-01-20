a07baaf9c2682ec3f7b45b3cea2bae61b7cc915b
tmoechel-futura <t.moechel@futura-solutions.de>
2024-01-20 07:24:58 +0100

    implement get and insert

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([
@@ -8,9 +9,23 @@ function App() {
     { id: 2, title: "The Unicorn" },
   ]);
 
-  const handleCreateBook = (title) => {
-    var newBook = { title, id: books.length + 1 };
-    setBooks([...books, newBook]);
+  // the empty array causes the arrow function to be called only once
+  useEffect(() => {
+    fetchBooks();
+  },[])
+
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    
+    setBooks(response.data);
+  };
+
+  const handleCreateBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+    // var newBook = { title: response.data.title, id: response.data.id };
+    setBooks([...books, response.data]);
   };
 
   const handleDeleteBookById = (id) => {
@@ -36,6 +51,7 @@ function App() {
   return (
     <div>
       <BookCreate onCreate={handleCreateBook} />
+      <hr class="h-px my-4 mt-4 bg-gray-200 border-0 dark:bg-gray-700" />
       <BookList
         books={books}
         onDelete={handleDeleteBookById}
