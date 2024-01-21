722883bb101279f36c9f46a044f35c850e55ae69
tmoechel-futura <t.moechel@futura-solutions.de>
2024-01-21 11:05:24 +0100

    cleanup new branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import { useEffect, useContext } from "react";
-
-import BookCreate from "./components/BookCreate";
-import BookList from "./components/BookList";
-import BookContext from "./context/BookContext";
-
 function App() {
-  const { fetchBooks } = useContext(BookContext);
-
-  // the empty array causes the arrow function to be called only once
-  // if the arrow function inside useEffect references a variable this variable can become stale
-  useEffect(() => {
-    fetchBooks();
-  }, [fetchBooks]);
-
   return (
     <div>
-      <BookCreate />
-      <hr className="h-px my-4 mt-4 bg-gray-200 border-0 dark:bg-gray-700" />
-      <BookList />
+      <h2> Hello React! </h2>
     </div>
   );
 }
--- src/hooks/useBooksContext.js
+++
@@ -1,9 +0,0 @@
-
-import { useContext } from "react";
-import BookContext from "../context/BookContext";
-
-function useBookContext() {
-    return useContext(BookContext);
-}
-
-export {useBookContext}
